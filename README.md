3c4f8b82616e344e918ca4092ac4bf7885c4f7d8
Matthew <2565361+mshafer1@users.noreply.github.com>
2020-11-08 16:07:29 -0600

    cache

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # get_parse_helpers
+
+A utility script for loading and parsing the query string on the current page - useful for static sites like those hosted on gh-pages.
+
---
+++ scripts/get_helpers.js
@@ -0,0 +1,81 @@
+DEBUG_LOGGING_ENABLED = false;
+
+function _debug_log() {
+    if (!DEBUG_LOGGING_ENABLED) {
+        return; // NO-OP
+    }
+
+    console.debug(...arguments);
+}
+
+function escapeRegExp(str) { // from https://stackoverflow.com/a/1144788/8100990
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function _load_get(location) {
+    var result = {}
+
+    // enable debug logging if asked
+    if (location.includes('ENABLE_DEBUG_LOGGING')) {
+        DEBUG_LOGGING_ENABLED = true;
+    }
+
+    if (location.indexOf('?') == -1) {
+        _debug_log("No query string");
+        return result;
+    }
+    var query = location
+        // get the query string
+        .replace(/^.*?\?/, '')
+        // and remove any existing hash string (thanks, @vrijdenker)
+        .replace(/#.*$/, '')
+        .replace(new RegExp(escapeRegExp(' '), 'g'), '')
+        .split('&');
+
+    for (var i = 0, l = query.length; i < l; i++) {
+        aux = decodeURIComponent(query[i])
+        console.log(aux)
+        key = aux.match(/([\d\D]+?\=\s?)/)[0].replace('=', '')
+        // _debug_log(key)
+        value = aux.replace(key + "=", "").replace(new RegExp(escapeRegExp('+'), 'g'), ' ')
+        // clean up the key
+        key = key.replace(/^[\s]*/g, "").replace(/\+/g, " ")
+        // _debug_log(value)
+        if (key in result) {
+            if (result[key].constructor === Array) {
+                result[key].push(value)
+            } else {
+                result[key] = [result[key], value]
+            }
+        } else {
+            if (key.includes('[]')) {
+                _debug_log("Array detected")
+                result[key] = [];
+                result[key].push(value)
+            } else {
+                result[key] = value;
+            }
+            _debug_log(key + ":" + result[key])
+            _debug_log();
+        }
+    }
+
+    return result;
+}
+
+//originally from https:///stackoverflow.com/a/12049737
+function load_get() {
+    GET = _load_get(document.location.toString());
+    return GET;
+}
+
+try {
+    // from https://stackoverflow.com/a/11279639
+    // if module is availble, we must be getting included via a 'require', export methods
+    var exports = module.exports = {};
+
+    exports._load_get = _load_get;
+    exports.load_get = load_get;
+} catch (error) {
+    // pass
+}
